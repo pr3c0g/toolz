62f92e0d7afe16d9595f18d8f59e6997f1e39222
pr3c0g <precogster@gmail.com>
2021-07-21 13:43:03 +0100

    - Added ansi coloring - Added better handling of failing to ssh even if ping works

--- rssh.sh
+++ rssh.sh
@@ -1,15 +1,15 @@
 #!/usr/local/bin/bash
 
 wait_for_ping() {
-  trap '{ echo "Cancelled!" ; exit 1; }' INT  
-  echo -n "Pinging" "$host" "."
+  trap '{ echo -e "\033[0;31mCancelled!\033[0;00m" ; exit 1; }' INT  
+  echo -en "\033[0;33mPinging $host .\033[0;00m"
   # because of -t 1, ping fails to trap internally, triggering SIGALRM, so we need to trap that
   trap '{echo "alarm"}' SIGALRM 
   until ping -t 1 "${host//.*}" &> /dev/null; do
     echo -n "."
     sleep 1
   done
-  echo " ok!"
+  echo -e "\033[0;33m ok!\033[0;00m"
 }
 
 check_ssh(){
@@ -21,8 +21,11 @@ main() {
   host="${@: -1}"
   while true; do
     wait_for_ping "$@"
-    check_ssh "$host" && ssh "$@"; exit 0
-    echo "Can't ssh, checking ping again .."
+    if check_ssh "$host"; then
+      ssh "$@"
+      [[ $? == 0 ]] && exit 0
+    fi
+    echo -en "\033[0;33mChecking ping again ... \033[0;00m"
   done
 }
 
